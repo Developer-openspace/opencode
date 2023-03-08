8fe663cab0c018e90829df4024bb1008091644dc
imrany <imranmat254@gmail.com>
2023-03-08 20:40:51 +0300

    used grid but I'm change to mansory layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 </svelte:head>
 
 <main class="m-6 text-lg flex justify-center">
-    <div class="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-1">
+    <div class="grid grid-cols-1 sm:grid-cols-3">
         <img src="/comp-with-code.jpg" class="w-25 " alt="image1"/>
         <img src="/sleep.jpg" class="w-25 " alt="image2"/>
         <img src="/figure.jpg" class="w-25 " alt="image3"/>
