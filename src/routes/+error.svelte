11c113535e0a2488dddcc58d7d38163deab3000c
imrany <imranmat254@gmail.com>
2023-03-08 19:43:45 +0300

    added the 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+    <title>Open code | Not found</title>
+</svelte:head>
+
+<main class="flex flex-col justify-center items-center m-12">
+    <p class="text-3xl text-gray-700">Page not found</p>
+    <a href="/" class="my-5">
+        <button class="p-2 border rounded-lg">Return Home</button>
+    </a>
+</main>
