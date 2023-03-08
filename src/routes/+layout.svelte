78c2d9a75ea0b7f709286759c38104e98f7fbe90
imrany <imranmat254@gmail.com>
2023-03-08 19:46:11 +0300

    working the landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
-    import "./app.css"
+    import { assets } from "$app/paths";
+import "./app.css"
 </script>
 
-<nav class="">
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/blog/hello-world">Hello</a>
+<nav class="p-5 text-xl shadow flex">
+    <img src="/open-code.png" class="w-10 h-7 shadow-sm" alt="logo"/>
+    <a class="mx-2" href="/">Home</a>
+    <a class="mx-2" href="/about">About</a>
+    <a class="mx-2" href="/blog/hello-world">Hello</a>
 </nav>
   
-<slot></slot>
+<slot class="mx-1"></slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href='/about' class="text-grey-100 flex ">about us</a>
+
+<svelte:head>
+    <title>Open code | Home</title>
+</svelte:head>
+
+<main class="m-6 text-lg flex justify-center">
+    <div class="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-1">
+        <img src="/comp-with-code.jpg" class="w-25 " alt="image1"/>
+        <img src="/sleep.jpg" class="w-25 " alt="image2"/>
+        <img src="/figure.jpg" class="w-25 " alt="image3"/>
+        <img src="/marketing.jpg" class="w-25 " alt="image4"/>
+        <img src="/minion.jpg" class="w-25 " alt="image5"/>
+        <img src="/minons1.jpg" class="w-25 " alt="image6"/>
+        <img src="" alt=""/>
+        <img src="" alt=""/>
+
+    </div>
+</main>
