e3e684fea3ba8d6e7854c411af1d52df40915360
imrany <imranmat254gmail.com>
2023-03-11 11:44:18 +0300

    working on the project detail page data

--- src/routes/project/[id]/+page.svelte
+++ src/routes/project/[id]/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-
+    /** @type {import('./$types').PageData} */  
+    export let data;
 </script>
 
-<main>
-    <p>About project</p>
+<main class="m-6 text-lg flex flex-col">
+    <p>{data.id} About project</p>
 </main>
