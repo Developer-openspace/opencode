a606c27a7638f058daaac69d2ed2925c788ac23e
imrany <imranmat254gmail.com>
2023-03-12 21:36:29 +0300

    added lib folder to house components and db

---
+++ src/lib/db/data.js
@@ -0,0 +1,68 @@
+export const content=[
+    {
+        "name":"School management",
+        "desc":"this is a school management that manages school records and files.",
+        "image":"/comp-with-code.jpg",
+        "alt":"image1"
+    },
+    {
+        "name":"Hostel management",
+        "desc":"This is a hostel management.",
+        "image":"/sleep.jpg",
+        "alt":"image2"
+    },
+    {
+        "name":"Booking management",
+        "desc":"This is a booking management.",
+        "image":"/figure.jpg",
+        "alt":"image3"
+    },
+    {
+        "name":"Hostel management",
+        "desc":"this is a hotel management.",
+        "image":"/marketing.jpg",
+        "alt":"image4"
+    },
+    {
+        "name":"Health management",
+        "desc":"This is a health",
+        "image":"/minion.jpg",
+        "alt":"image5"
+    },
+    {
+        "name":"Leave management",
+        "desc":"This is a leave management",
+        "image":"/minons1.jpg",
+        "alt":"image6"
+    },
+    {
+        "name":"Loan management",
+        "desc":"This is a loat that give out loans to user per number of savings and amount of savings.",
+        "image":"/comp-with-code1.jpg",
+        "alt":"image7"
+    },
+    {
+        "name":"Bank management",
+        "desc":"This is a banking system that organises data into clusters and collections",
+        "image":"/comp-with-code2.jpg",
+        "alt":"image8"
+    },
+    {
+        "name":"Campus management",
+        "desc":"This is a campus management development on nodejs",
+        "image":"/robo-lego.jpg",
+        "alt":"image9"
+    },
+    {
+        "name":"Medical assistance chat bot",
+        "desc":"This is a medical assistance, was created with a machine learning library, it performs google queries for the user/",
+        "image":"/comp-with-code4.jpg",
+        "alt":"image10"
+    },
+    {
+        "name":"Management",
+        "desc":"This is a simple management",
+        "image":"/comp-with-code3.jpg",
+        "alt":"image11"
+    }
+]
