db6ff500fc8c8c35f6b2effa070670492c290de8
imrany <imranmat254gmail.com>
2023-03-12 21:36:54 +0300

    working on the landing page

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,10 @@
-import * as db from '../db/data.json';
 import { error } from '@sveltejs/kit';
  
 /** @type {import('./$types').PageLoad} */
-export async function load() {
-    return new Response({text:"Hello there"})
+export async function load({fetch}) {
+  const url=`/api/projects`;
+  const response=await fetch(url);
+  const parseRes=await response.json();
+  return { parseRes }
   throw error(404, 'Not found');
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 
 <main class="m-6 text-lg flex justify-center">
     <div class="grid-cols-1 sm:columns-3 sm:gap-2">
-        <!-- {#each data.content as item }
+        {#each data.parseRes as item }
             <div class="mb-16">
                 <a href={`/project/${item.image}`} >
                     <img src={item.image} class="w-full rounded-[15px] shadow-lg" alt={item.alt}/>
@@ -19,7 +19,6 @@
                     </div>
                 </a>
             </div>
-        {/each} -->
-        <p>{data}</p>
+        {/each}
     </div>
 </main>
