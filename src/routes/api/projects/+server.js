0002f3081c857d78fb9053906e28635be0294d09
imrany <imranmat254gmail.com>
2023-03-13 18:38:13 +0300

    add post api route handler

--- src/routes/api/projects/+server.js
+++ src/routes/api/projects/+server.js
@@ -1,6 +1,16 @@
 import {content} from "$lib/db/data";
+import {projects} from "$lib/mongodb/models/project_model";
 import {json} from "@sveltejs/kit";
 
+//get all projects
 export function GET(){
     return json(content);
+}
+
+//post (add project to db)
+export async function POST(RequestEvent){
+    const project=RequestEvent;
+    const items=project.request.body;
+    const insertProject=await projects.insertOne({items})
+    return json(insertProject,{status:201});
 }
