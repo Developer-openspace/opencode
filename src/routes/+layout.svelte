61dd38cdd16a3f0d5fc35fdbe83a88da07d8297f
imrany <imranmat254gmail.com>
2023-03-14 11:55:20 +0300

    added more links on the navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,15 @@
 import "./app.css"
 </script>
 
-<nav class="p-5 text-xl shadow flex">
-    <img src="/open-code.png" class="w-10 h-7 shadow-sm" alt="logo"/>
-    <a class="mx-2" href="/">Home</a>
-    <a class="mx-2" href="/about">About</a>
-    <a class="mx-2" href="/blog/hello-world">Hello</a>
+<nav class="p-5 text-xl shadow flex justify-between">
+    <div class="flex">
+        <img src="/open-code.png" class="w-10 h-7 rounded-sm shadow-sm mr-2" alt="logo"/>
+        <a class="mx-2" href="/">Explore</a>
+        <a class="mx-2" href="/about">About</a>
+        <a class="mx-2" href="/projects/add">Add</a>
+        <a class="mx-2" href="/sign_in">Sign in</a>
+    </div>
+    <a class="ml-20 px-2 py-1 text-base text-white rounded-md border-slate-600 bg-slate-500" href="/get_started">Get Started</a>
 </nav>
   
 <slot class="mx-1"></slot>
