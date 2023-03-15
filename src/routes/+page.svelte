e7319de1122d55e80b856e7751609ba208388f87
imrany <imranmat254gmail.com>
2023-03-15 12:53:34 +0300

    new changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,7 @@
 <script>
-    /** @type {import('./$types').PageData} */  
-    export let data;
+
 </script>
 
-<svelte:head>
-    <title>Open code | Home</title>
-</svelte:head>
-
-<main class="m-6 text-lg flex justify-center">
-    <div class="grid-cols-1 sm:columns-3 sm:gap-2">
-        {#each data.parseRes as item }
-            <div class="mb-16">
-                <a href={`/projects/${item.count}`} >
-                    <img src={item.image} class="w-full rounded-[15px] shadow-lg" alt={item.alt}/>
-                    <div class="mt-[-80px] h-[30px] text-gray-100 ml-8">
-                        <p class="text-xl font-semibold">{item.name}</p>
-                        <p class="text-lg">{item.desc.slice(0,20)}</p>
-                    </div>
-                </a>
-            </div>
-        {/each}
-    </div>
+<main>
+    <h1>Welcome to opencode</h1>
 </main>
