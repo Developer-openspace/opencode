9acad42a30f6b55afd92a3c1d5792bf3305c9d59
imrany <imranmat254gmail.com>
2023-03-15 14:54:20 +0300

    changes on the navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
         <img src="/open-code.png" class="w-10 h-7 rounded-sm shadow-sm mr-2" alt="logo"/>
         <a class="mx-2" style="font-family: 'Tilt Neon';" href="/">OpenCode</a>
     </div>
-    <div class="flex">
+    <div class="sm:flex none">
         <a class="mx-2" href="/explore">Explore</a>
         <a class="mx-2" href="/about">About</a>
         <a class="mx-2" href="/projects/add">Add</a>
