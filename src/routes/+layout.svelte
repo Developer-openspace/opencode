7d129fb56167f527851f0cd233706dbd345ba4fa
imrany <imranmat254gmail.com>
2023-03-15 13:56:46 +0300

    arranged nav links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,15 @@
 <nav class="p-5 text-xl shadow flex justify-between">
     <div class="flex">
         <img src="/open-code.png" class="w-10 h-7 rounded-sm shadow-sm mr-2" alt="logo"/>
+        <a class="mx-2" style="font-family: 'Tilt Neon';" href="/">OpenCode</a>
+    </div>
+    <div class="flex">
         <a class="mx-2" href="/explore">Explore</a>
         <a class="mx-2" href="/about">About</a>
         <a class="mx-2" href="/projects/add">Add</a>
         <a class="mx-2" href="/sign_in">Sign in</a>
+        <a class="ml-4 px-2 py-1 text-base text-white rounded-md border-slate-600 bg-slate-500 " href="/get_started">Get Started</a>
     </div>
-    <a class="ml-20 px-2 py-1 text-base text-white rounded-md border-slate-600 bg-slate-500" href="/get_started">Get Started</a>
 </nav>
   
 <slot class="mx-1"></slot>
