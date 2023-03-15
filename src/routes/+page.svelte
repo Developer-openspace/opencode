14aa252837c378f0de3f31c2438e19e722dc1487
imrany <imranmat254gmail.com>
2023-03-15 14:56:04 +0300

    working on the hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,13 @@
 <svelte:head>
     <title>Open code | Home</title>
 </svelte:head>
-<main class="flex flex-col items-center">
-    <h1>Welcome to opencode</h1>
+<main class="text-xl py-5 flex flex-col items-center">
+   <div class="flex flex-col items-center mt-[4rem]">
+        <div class="text-center text-xl font-medium sm:text-3xl uppercase mb-3">
+            <p>OpenCode provides Access to different <span class="text-slate-500">Computer</span></p>
+            <p><span class="text-slate-500">science</span> projects.</p>
+        </div>
+        <p class="text-center text-slate-700 text-lg max-sm:w-[70vw]">Built for students, explore and download free and paid projects easily.</p>
+        <a href="" class="m-5 p-2 hover: bg-slate-500 text-white rounded-md capitalize">View all projects</a>        
+   </div>
 </main>
