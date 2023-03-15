703681d4533f152cce07308424ccfe1915a140ed
imrany <imranmat254gmail.com>
2023-03-15 19:43:35 +0300

    working on the post handler

--- src/routes/projects/add/+page.svelte
+++ src/routes/projects/add/+page.svelte
@@ -1,6 +1,32 @@
 <script>
-     /** @type {import('./$types').PageData} */  
-     export let data;
+// @ts-nocheck
+    import { redirect } from "@sveltejs/kit";
+
+    let name, image, desc;
+      async function add(e){
+        try {
+            e.preventdefault()
+            const url=`/api/projects/add`
+            const response=await fetch(url,{
+                method:"POST",
+                body:JSON.stringify({
+                    name,
+                    image,
+                    desc
+                }),
+                headers:{
+                    'content-type':"applictaion/json"
+                }
+            })
+            const parseRes=await response.json();
+            if(parseRes.msg){
+                console.log(parseRes.msg);
+                redirect(200,'/');
+            }
+        } catch (error) {
+            redirect(404,'/')
+        }
+     }
 </script>
 
 <svelte:head>
@@ -8,11 +34,13 @@
 </svelte:head>
 
 <main class="flex flex-col items-center justify-center">
-    <form method="POST" class="flex flex-col my-10 text-lg w-[90%] sm:w-[70%]" action="/projects/add">
-        <label>Name</label>
-        <input type="text" placeholder="Provide the appropriate name of your project" title="Provide the appropriate name of your project" class="border-slate-500 rounded-lg bg-slate-100 p-2 placeholder:text-slate-600" required maxlength="100" name="name"/>        
-        <label class="mt-4">Description</label>
-        <textarea name="desc" minlength="100" class="border-slate-500 rounded-lg p-2 bg-slate-200 placeholder:text-slate-600 h-[40vh]" placeholder="What's the project about?" title="What's the project about?" required></textarea>
+    <form on:submit={add} class="flex flex-col my-10 text-lg w-[90%] sm:w-[70%]">
+        <label for="name">Name</label>
+        <input type="text" bind:value={name} id="name" placeholder="Provide the appropriate name of your project" title="Provide the appropriate name of your project" class="border-slate-500 rounded-lg bg-slate-100 p-2 placeholder:text-slate-600" required maxlength="100" name="name"/>   
+        <label for="image">Image</label>
+        <input type="text" bind:value={image} id="image" placeholder="Provide image path" title="Provide the appropriate name of your project" class="border-slate-500 rounded-lg bg-slate-100 p-2 placeholder:text-slate-600" required maxlength="100" name="image"/>             
+        <label class="mt-4" for="desc">Description</label>
+        <textarea name="desc" bind:value={desc} id="desc" minlength="100" class="border-slate-500 rounded-lg p-2 bg-slate-200 placeholder:text-slate-600 h-[40vh]" placeholder="What's the project about?" title="What's the project about?" required></textarea>
         <button class="bg-green-800 mt-2 border-none text-white rounded-lg px-2 py-2">Submit</button>
     </form>
 </main>
