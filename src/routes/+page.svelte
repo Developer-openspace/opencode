e23c725027740752ca3969cd6010291375a5499b
imrany <imranmat254gmail.com>
2023-03-15 13:36:19 +0300

    trial failed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,6 @@
 <svelte:head>
     <title>Open code | Home</title>
 </svelte:head>
-<main class="flex flex-col items-center">
+<main class="flex flex-col items-center py-4 text-lg">
     <h1>Welcome to opencode</h1>
 </main>
