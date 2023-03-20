296df09ebef53ffed8b5360101ba579fef157bab
imrany <imranmat254gmail.com>
2023-03-21 02:49:10 +0300

    populating dynamic content

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -13,13 +13,13 @@
         {#each data.parseRes as item }
             <div class="mb-16" id={item.id}>
                 <a href={`/repos/${item.name}`} >
-                    <img src={item.image} class="w-full rounded-[15px] shadow-lg" alt={item.alt}/>
+                    <img src="/static/comp-with-code.jpg" class="w-full rounded-[15px] shadow-lg" alt={item.alt}/>
                     <div class="mt-[-80px] h-[30px] text-gray-100 ml-8">
                         <p class="text-xl font-semibold">{item.full_name}</p>
-                        <p class="text-lg">{item.description.slice(0,20)}</p>
+                        <p class="text-lg">{item.description}</p>
                         <div class="flex it">
                             <a href={`${item.owner.html_url}`} target="_blank" rel="noreferrer">
-                                <img class="w-[50px] h-[50px] rounded-lg" src={item.owner.avatar_url} alt={item.owner.login}/>
+                                <img class="w-[50px] h-[50px] rounded-[50px]" src={item.owner.avatar_url} alt={item.owner.login}/>
                             </a>
                             <a href={`${item.clone_url}`} download>Download repo</a>
                             <a href={`${item.forks_url}`} target="_blank" rel="noreferrer">Fork repo</a>
