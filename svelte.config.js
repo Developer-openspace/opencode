fb4ef11652534c14d4e58e5445f25b694033ab94
imrany <imranmat254gmail.com>
2023-03-21 11:38:38 +0300

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import node from "@sveltejs/adapter-node";
+import vercel from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: node(),
+    adapter: vercel(),
     csrf: {
       checkOrigin: false,
     }
